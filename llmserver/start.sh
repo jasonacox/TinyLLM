69830cf03518dec198253bed1b7168d9b00da184
Jason A. Cox <jason@jasonacox.com>
2023-10-22 21:55:00 -0700

    Disable interrupt_requests for multi-threading

--- llmserver/start.sh
+++ llmserver/start.sh
@@ -7,7 +7,19 @@ echo "Starting llama_cpp.server for $MODEL --n_gpu_layers $N_GPU_LAYERS --host $
 CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip3 install llama-cpp-python
 CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip3 install llama-cpp-python[server]
 
-# Start server
-python3 -m llama_cpp.server --model "$MODEL" --n_gpu_layers $N_GPU_LAYERS --port $PORT
+# Configure server
+MODEL=llama-2-7b-chat.Q5_K_M.gguf  # Use the llama-2-7b-chat model
+                                   # (mistral-7b-instruct-v0.1.Q5_K_M.gguf)
+HOST=0.0.0.0                       # Listen on all interfaces
+PORT=5000                          # Listen on port 5000
+N_GPU_LAYERS=32                    # Use 32 GPU layers
 
-echo "Stop"
+# Start server
+python3 -m llama_cpp.server \
+    --model "$MODEL" \
+    --host $HOST \
+    --port $PORT \
+    --interrupt_requests False \
+    --n_gpu_layers $N_GPU_LAYERS
+
+echo "Started... listening on http://$HOST:$PORT"
